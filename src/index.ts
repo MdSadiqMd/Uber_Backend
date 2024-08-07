815889bcdd54b0690ec64811304d946e51bc1ff6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-08-07 21:27:55 +0530

    Feat: Added bosy parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import bodyParser from "body-parser";
 import express, { Express } from "express";
 
 import serverConfig from "./config/server.config";
 import logger from "./config/logger.config";
 
 const app: Express = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
 app.listen(serverConfig.PORT, async () => {
     logger.info(`Server started on PORT: ${serverConfig.PORT}`);
